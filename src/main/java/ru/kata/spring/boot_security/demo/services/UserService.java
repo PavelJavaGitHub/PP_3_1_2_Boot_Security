b1088dd5b8999d19949e2817804e7abd021046e8
PavelJavaGitHub <105481448+PavelJavaGitHub@users.noreply.github.com>
2022-06-21 14:54:13 +0300

    another not working version #3

--- src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
@@ -6,19 +6,55 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ru.kata.spring.boot_security.demo.dao.UserDao;
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+import java.util.List;
 
 @Service
 public class UserService implements UserDetailsService {
 
-    private final UserDao userDao;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     @Autowired
-    public UserService(UserDao userDao) {
-        this.userDao = userDao;
+    public UserService(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public User get(long id) {
+        return userRepository.getById(id);
+    }
+
+    public User getByEmail(String email) {
+        return userRepository.findByEmail(email).orElse(null);
+    }
+
+    public void update(long id, User user) {
+        save(user);
+    }
+
+    public void delete(long id) {
+        userRepository.deleteById(id);
+    }
+
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userDao.getByEmail(username);
+        return userRepository.findByEmail(username).orElse(null);
     }
 }
